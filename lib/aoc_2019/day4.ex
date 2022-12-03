bd32084ae2b2f4e2f14ce02ad637aeee47023354
Tim Huddle <blazhen88@gmail.com>
2022-12-03 02:14:11 -0500

    day4

---
+++ lib/aoc_2019/day4.ex
@@ -0,0 +1,86 @@
+defmodule Aoc2019.Day4 do
+  @moduledoc false
+
+  @doc ~S"""
+    iex> part1("111111-111111")
+    1
+    iex> part1("223450-223450")
+    0
+    iex> part1("123789-123789")
+    0
+
+    iex> Aoc2019.load_data(4) |> part1()
+    1855
+  """
+  @spec part1(String.t()) :: integer()
+  def part1(input) do
+    input
+    |> prepare_input()
+    |> Stream.map(fn num -> repeats?(num) and increase?(num) end)
+    |> Enum.filter(&(&1))
+    |> Enum.count()
+  end
+
+  @doc ~S"""
+    iex> part2("112233-112233")
+    1
+    iex> part2("123444-123444")
+    0
+    iex> part2("111122-111122")
+    1
+
+    iex> Aoc2019.load_data(4) |> part2()
+    1253
+  """
+  @spec part2(String.t()) :: integer()
+  def part2(input) do
+    input
+    |> prepare_input()
+    |> Stream.map(fn num -> only_repeats_twice?(num) and increase?(num) end)
+    |> Enum.filter(&(&1))
+    |> Enum.count()
+  end
+
+  @spec prepare_input(String.t()) :: Range.t()
+  defp prepare_input(input) do
+    input
+    |> String.trim()
+    |> String.split("-", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> (fn [a, b] -> Range.new(a, b) end).()
+  end
+
+  defp repeats?(num) do
+    num
+    |> Integer.to_string()
+    |> String.graphemes()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.any?(fn [a, b] -> a == b end)
+  end
+
+  defp increase?(num) do
+    num
+    |> Integer.to_string()
+    |> String.graphemes()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a <= b end)
+  end
+
+  defp only_repeats_twice?(num) do
+    graphemes = num |> Integer.to_string() |> String.graphemes()
+
+    doubles = graphemes
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.filter(fn [a, b] -> a == b end)
+    |> Enum.map(&Enum.at(&1, 0))
+    |> Enum.uniq()
+
+    triples = graphemes
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.filter(fn [a, b, c] -> a == b and a == c end)
+    |> Enum.map(&Enum.at(&1, 0))
+    |> Enum.uniq()
+
+    not Enum.empty?(doubles) and (Enum.empty?(triples) or not Enum.all?(doubles, fn num -> Enum.member?(triples, num) end))
+  end
+end
---
+++ test/aoc_2019/day4_test.exs
@@ -0,0 +1,6 @@
+defmodule Aoc2019.Day4Test do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  doctest Aoc2019.Day4, import: true
+end
